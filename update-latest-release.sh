2099fdc334c17de0815ed84dc0eae21dbdee7825
Ihor Solodrai <ihor.solodrai@linux.dev>
2025-01-31 16:17:38 -0800

    Initial build-and-release workflow

---
+++ update-latest-release.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -euo pipefail
+
+GCC_BPF_INSTALLDIR=$1
+
+latest_release_tag=$(gh release list -L 1 -R theihor/gcc-bpf --json tagName -q .[].tagName)
+
+binutils_version=$(ls "${GCC_BPF_INSTALLDIR}" | grep "binutils.*built" | sed 's/.built//')
+gcc_version=$(ls "${GCC_BPF_INSTALLDIR}" | grep "gcc.*built" | sed 's/.built//')
+built_release_tag="${gcc_version}-${binutils_version}"
+
+if [[ "$latest_release_tag" == "$built_release_tag" ]]; then
+    echo "Release with tag $latest_release_tag already exists"
+    exit 0
+fi
+
+if [[ -z "$built_release_tag" ]]; then
+    echo "Could not determine built release tag"
+    exit 1
+fi
+
+cd $(dirname $(realpath $GCC_BPF_INSTALLDIR))
+tar -cJvf ${built_release_tag}.tar.xz $(basename $(realpath $GCC_BPF_INSTALLDIR))
+
+# assume access to gh and upload a new release
+gh release create ${built_release_tag} ./${built_release_tag}.tar.xz -p
+
